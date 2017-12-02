7cbc03ae3800615944c64bffc5b8fda4ca0af9c3
Axel Kummer <kummeraxel@gmail.com>
2017-12-02 12:14:00 +0100

    Fix unittests

--- tests/Model/LogEntryTest.php
+++ tests/Model/LogEntryTest.php
@@ -37,7 +37,7 @@ class LogEntryTest extends TestCase
         $this->assertSame("LogEntry Test", $logEntry->getMessage());
         $this->assertSame($context, $logEntry->getContext());
 
-        $strExpected = "{\"time\":".time().",\"message\":\"LogEntry Test\",\"severity\":\"notice\",\"context\":{\"debug\":{\"test\":1,\"test2\":2}}}";
+        $strExpected = "{\"timestamp\":".time().",\"message\":\"LogEntry Test\",\"severity\":\"notice\",\"context\":{\"debug\":{\"test\":1,\"test2\":2}}}";
         $strCurrent  = (string) $logEntry;
 
         $this->assertSame($strExpected, $strCurrent);
@@ -65,7 +65,7 @@ class LogEntryTest extends TestCase
         $this->assertSame(LogLevel::NOTICE, $logEntry->getSeverity());
         $this->assertSame("LogEntry Test", $logEntry->getMessage());
 
-        $strExpected = "{\"time\":".time().",\"message\":\"LogEntry Test\",\"severity\":\"notice\",\"context\":{\"debug\":{\"objectType\":\"stdClass\",\"test\":1,\"test2\":2}}}";
+        $strExpected = "{\"timestamp\":".time().",\"message\":\"LogEntry Test\",\"severity\":\"notice\",\"context\":{\"debug\":{\"objectType\":\"stdClass\",\"test\":1,\"test2\":2}}}";
         $strCurrent  = (string) $logEntry;
 
         $this->assertSame($strExpected, $strCurrent);
@@ -84,7 +84,7 @@ class LogEntryTest extends TestCase
         $this->assertSame(LogLevel::NOTICE, $logEntry->getSeverity());
         $this->assertSame("LogEntry Test", $logEntry->getMessage());
 
-        $strExpected = "{\"time\":".time().",\"message\":\"LogEntry Test\",\"severity\":\"notice\",\"context\":{\"debug\":{\"objectType\":\"AxelKummer\\\\LogBook\\\\Request\\\\HttpRequest\",\"appIdentifier\":\"test\",\"host\":\"test\",\"port\":\"test\"}}}";
+        $strExpected = "{\"timestamp\":".time().",\"message\":\"LogEntry Test\",\"severity\":\"notice\",\"context\":{\"debug\":{\"objectType\":\"AxelKummer\\\\LogBook\\\\Request\\\\HttpRequest\",\"appIdentifier\":\"test\",\"host\":\"test\",\"port\":\"test\"}}}";
         $strCurrent  = (string) $logEntry;
 
         $this->assertSame($strExpected, $strCurrent);
